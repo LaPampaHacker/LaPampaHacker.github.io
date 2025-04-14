d8cfe265e16f1542d1683a0108e7f447435e2be0
LaPampaHacker <joaquinhernandezcacu@gmail.com>
2025-04-13 22:05:10 -0300

    a

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,10 +13,9 @@ export function formatDate(date: Date) {
   }).format(date)
 }
 
-export function readingTime(html: string): string {
-  const textOnly = html.replace(/<[^>]*>/g, " ").trim()
-  const wordCount = textOnly.split(/\s+/).filter(Boolean).length
-  const wordsPerMinute = 200
-  const readingTimeMinutes = Math.max(1, Math.round(wordCount / wordsPerMinute))
+export function readingTime(html: string) {
+  const textOnly = html.replace(/<[^>]+>/g, "")
+  const wordCount = textOnly.split(/\s+/).length
+  const readingTimeMinutes = ((wordCount / 200) + 1).toFixed()
   return `${readingTimeMinutes} min read`
 }
